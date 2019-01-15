62fd4eb76ca81d9ff75245c7b835b2fc60b3655d
Cody Troop <codytroop@yahoo.com>
2019-01-15 15:29:41 -0600

    Header appears on DOM

---
+++ src/components/FamousPerson/FamousPerson.js
@@ -0,0 +1,17 @@
+// import React, {Component} from 'react';
+
+
+// class FamousPerson extends Component {
+//     constructor() {
+
+//     }
+//     render(){
+//         return(
+//             <div className='header'>
+//                 <h2>App Name!</h2>
+//             </div>
+//         )
+//     }
+// }
+
+// export default FamousPerson;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render(){
+        return(
+            <div className='header'>
+                <h2>App Name!</h2>
+            </div>
+        )
+    }
+}
+
+export default Header;
